87a6e20834485a6be965e484e4dab665f3786e10
SONGGUOPENG <sandhooking@outlook.com>
2025-01-02 01:09:53 +0800

    Create tradingback.py

---
+++ tradingback.py
@@ -0,0 +1,10 @@
+#2025.01.01 SONGGUOPENG
+#Microsoft Skype：https://join.skype.com/invite/vMnpR1gq7giA
+"""
+策略仪：回测
+https://drive.google.com/file/d/1MBs3kiSAHtiDr-_ho_wKsa5-ZTlHjjT1/view?usp=drivesdk
+"""
+"""
+币安策略仪
+https://github.com/SONGGUOPENG2/BinanceTrader/releases
+"""
